1271e6c33783208ca56b087200edb53e739fee0d
Joel Mendonsa <100001521+RAPZ0D@users.noreply.github.com>
2023-11-17 19:43:27 -0500

    Updated tfa.py
    This is the new two factor authenticaton page using flet library

--- tfa.py
+++ tfa.py
@@ -1,35 +1,9 @@
 import flet as ft
-import smtplib
-import random
-import string
-from email.message import EmailMessage
-def generate_code(length=6):
-    # Generate a random code of specified length
-    return ''.join(random.choices(string.digits, k=length))
+from mailing import verification_code
 
-def send_email(receiver_email, code):
-    sender_email = 'joel.mendonsa30@gmail.com'  # Your Gmail address
-    sender_password = 'pkhymedcjtuwnied'  # Your generated App Password
-    
-    subject = "Two Factor Authorization Code"
-    body = f"This is your Two Factor Authoriation code. Do not share it with anyone.\nYour Code is: {code}"
-
-    message = EmailMessage()
-    message['From'] = sender_email
-    message['To'] = receiver_email
-    message['Subject'] = subject
-    message.set_content(body)
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-        server.login(sender_email, sender_password)
-        server.send_message(message)
-
-# Usage example:
-user_email = 'joel.edwin05@gmail.com'  
-verification_code = generate_code()
-send_email(user_email, verification_code)
 
 def main(page):
+    page.title = "TWO FACTOR AUTHENTICATION"
     def btn_click(e):
         if not code.value:
             code.error_text = "Please enter the Verification Code"
@@ -37,7 +11,7 @@ def main(page):
         else:
             if verification_code == str(code.value):
                 page.clean()
-                page.add(ft.Text("Welcome to the chatbot"))
+                #page.add(ft.Text("Welcome to the chatbot"))
             elif verification_code != str(code.value): 
                 code.error_text = 'Wrong code entered \nEnter the correct code'
                 page.update()
